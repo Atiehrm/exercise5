a22be22da1add843c925a07136f060924aa4900f
Atieh Rahbari Manesh <61627824+Atiehrm@users.noreply.github.com>
2021-09-21 15:35:46 +0430

    Update Date.java

--- Date.java
+++ Date.java
@@ -4,16 +4,18 @@ import java.util.GregorianCalendar;
 public class Date {
     private final int minYear = 1;
     private final int maxYear = 9999;
-    private int day;
-    private int year;
-    private int month;
+    private int day = 11;
+    private int year = 1400;
+    private int month = 1;
     private final String[] months = {"farvardin", "ordibehesht", "khordad", "tir", "mordad", "shahrivar"
             , "mehr", "aban", "azar", "dey", "bahman", "esfand"};
 
     public Date(int year, int month, int day) {
-        this.year = year;
-        this.month = month;
-        this.day = day;
+        if (isValidDate(year, month, day)) {
+            this.year = year;
+            this.month = month;
+            this.day = day;
+        }
     }
 
     public Date() {
@@ -72,24 +74,24 @@ public class Date {
             date.setYear(date1.getYear());
             date.setMonth(date1.getMonth() + 1);
             date.setDay(1);
-        }
-        else if (date.getDay() == 30) {
-                if (date.getMonth() >= 7 || date.getMonth() <= 11) {
+        } else if (date.getDay() == 30) {
+            if (date.getMonth() >= 7 || date.getMonth() <= 11) {
                 date.setYear(date1.getYear());
                 date.setMonth(date1.getMonth() + 1);
                 date.setDay(1);
+
             } else if (date.getMonth() == 12) {
                 date.setYear(date1.getYear() + 1);
                 date.setMonth(1);
                 date.setDay(1);
             }
+
         } else {
             date.setYear(date1.getYear());
             date.setMonth(date1.getMonth());
             date.setDay(date1.getDay() + 1);
 
         }
-
         return date;
     }
 
@@ -102,4 +104,5 @@ public class Date {
                 '}';
     }
 }
+
 
